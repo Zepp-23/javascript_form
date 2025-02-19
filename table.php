9ff1b075caec744a238343365822725fc14013b3
COMLAB <>
2025-02-19 19:47:00 +0800

    COLSPAN.HYS
    HAHAHAHHA HIRAP

--- table.php
+++ table.php
@@ -7,6 +7,51 @@
 </head>
 <body>
     <h1>Student Record</h1>
-    <table id="Student Record"> </table>
+    <table id="Student_Record" border="1">
+
+    </table>
+
+    <script>
+        const StudentRecord = document.getElementById("Student_Record");
+
+        console.log("Hello World");
+
+        const row1 = StudentRecord.insertRow();
+
+        const row1col1 = row1.insertCell();
+        row1col1.innerHTML = "Last Name";
+
+        const row1col2 = row1.insertCell();
+        row1col2.innerHTML = "First Name";
+
+        const row2 = StudentRecord.insertRow();
+        const row2col1 = row2.insertCell();
+        const row2col2 = row2.insertCell();
+
+        const inputLastName = document.createElement("INPUT");
+        inputLastName.type = "text";
+        inputLastName.placeholder = "Enter Last Name";
+
+        row2col1.append (inputLastName);
+
+        const inputFirstName = document.createElement("INPUT");
+        inputFirstName.type = "text";
+        inputFirstName.placeholder = "Enter Last Name";
+
+        row2col2.append (inputFirstName);
+
+        const row3 = StudentRecord.insertRow();
+        const row3col1 = row3.insertCell();
+        row3col1.colSpan = 2;
+
+        const inputSubmit = document.createElement("INPUT");
+        inputSubmit.type = "submit"; 
+        inputSubmit.value = "Submit"; 
+
+        row3col1.append(inputSubmit);
+
+
+        console.log(StudentRecord);
+    </script>
 </body>
 </html>
